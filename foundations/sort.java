6c74e370a8ee7f151c0634909028b00609a8a5dc
marcalph <marcalph@protonmail.com>
2020-09-18 09:41:24 +0200

    add evenlySpaced

--- foundations/sort.java
+++ foundations/sort.java
@@ -5,6 +5,7 @@ public class sort{
     }
     public static void main(String[] args){
         System.out.println(Arrays.toString(method(new int[] {})));
-        System.out.println(Arrays.toString(method(new int[] {1})));System.out.println(Arrays.toString(method(new int[] {1,2,3,2,1})));
+        System.out.println(Arrays.toString(method(new int[] {1})));
+        System.out.println(Arrays.toString(method(new int[] {1,2,3,2,1})));
     }
 }
