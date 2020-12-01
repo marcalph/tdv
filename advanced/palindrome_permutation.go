c49d5e13c3adf47680e016a24bb192fb2b4bd0c5
marcalph <marcalph@protonmail.com>
2020-12-01 12:34:18 +0100

    permutation

--- advanced/palindrome_permutation.go
+++ advanced/palindrome_permutation.go
@@ -4,9 +4,20 @@ package main
 import "fmt"
 
 
-func permute(a []rune, s int, e int) {
+func count(s string) map[rune]int {
+	// create hashmap of char frequency from string
+	m := make(map[rune]int)
+    for _, r := range s {
+        m[r]++
+	}
+	return m
+}
+
+
+var perms []string
+func permute(a []rune, s int, e int) []string{
 	if s == e {
-		fmt.Println(a)
+		perms = append(perms, string(a))
 	} else {
 		for i := s; i <= e; i++ {
 			a[s], a[i] = a[i], a[s]
@@ -14,9 +25,52 @@ func permute(a []rune, s int, e int) {
 			a[s], a[i] = a[i], a[s]
 		}
 	}
+	return perms
 }
 
 
+func reverse(str string) (result string) { 
+    for _, v := range str { 
+        result = string(v) + result 
+    } 
+    return
+} 
+
+
 func main(){
-	permute([]rune("ab"),0, 1)
+	const s string = "abcabcd"
+	// m char freq counter for given string s
+	m := count(s)
+	// chars unique chars in given string
+	// cnt check if at most one char has a pair number of occurences
+	var runes []rune // runes w/ pair occurence
+	var pivot rune
+	cnt := 0
+	for c, f := range m {
+		if f % 2 != 0 {
+			cnt++
+			pivot = c
+			if f>1 {
+				for i:=0; i < f/2; i++{
+					runes = append(runes, c)
+				}	
+			}
+		}else{
+			for i:=0; i < f/2; i++{
+				runes = append(runes, c)
+			}
+		}
+	}
+	if cnt>1 {
+		fmt.Println("impossible")
+	} else {
+		fmt.Println("possible")
+		perms = permute(runes, 0, len(runes)-1)
+		fmt.Println(perms)
+		fmt.Println("results")
+		for _, v := range perms {
+			fmt.Println(v+string(pivot)+reverse(v))
+		}
+
+	}
 }
