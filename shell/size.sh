d45344ed7285c1cac01257f1e2a50ea2c9b7f934
marcalph <marcalph@protonmail.com>
2021-03-15 12:17:18 +0100

    shell

---
+++ shell/size.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+function size() {
+	result=
+    if [ -f "$1" ]; then
+        result=`du -sch $1`
+    else 
+        if [ -d "$1" ]; then
+            result=`tree $1 --du -h`
+        fi
+    fi
+    echo "$result"
+}
+
+size $1
